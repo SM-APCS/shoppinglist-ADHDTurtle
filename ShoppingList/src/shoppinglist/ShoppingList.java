60707eb212615a0fbf42537690021e20498416cf
Ivan Day <ivanday9@gmail.com>
2015-12-17 11:44:55 -0600

    Pray to god this works

--- ShoppingList/src/shoppinglist/ShoppingList.java
+++ ShoppingList/src/shoppinglist/ShoppingList.java
@@ -1,5 +1,8 @@
 package shoppinglist;
 
+
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author change to your name or loose 5 points
@@ -10,7 +13,19 @@ public class ShoppingList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // code application logic here
+        ArrayList shoppingList = new ArrayList();
+        boolean shop = true;
+        Scanner scan = new Scanner(System.in);
+        
+        while (shop == true){
+            System.out.println("Add an item to the list or type quit to quit");
+            String item = scan.nextLine();
+            if (item == "quit"){
+                shop = false;
+            } else shoppingList.add(item);
+            shoppingList.add(item);
+        }
+        
     }
     
 }
